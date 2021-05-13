0d2cd39e705cb3c3db590055e3b820515779d1d1
unknown <sridhantrahul@gmail.com>
2021-05-13 14:33:17 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from './Banner'
+import About from './About'
+
+function App() {
+  return (
+     <>
+
+          <Banner/>
+
+          <About/>
+
+     </>
+  );
+}
+
+export default App;
